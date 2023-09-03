2a17d9bb6f2c7feccc7303311094269fbc0c7658
DESKTOP-QD6PISA\W10 <leandro.prates@gmail.com>
2023-09-03 11:26:48 -0300

    habilita cors

--- src/main/java/com/example/projetoahoybackend/controller/BuscaController.java
+++ src/main/java/com/example/projetoahoybackend/controller/BuscaController.java
@@ -6,11 +6,13 @@ import com.example.projetoahoybackend.service.BuscaDadosService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class BuscaController {
 
     @Autowired
--- src/main/java/com/example/projetoahoybackend/controller/SalvarArquivoController.java
+++ src/main/java/com/example/projetoahoybackend/controller/SalvarArquivoController.java
@@ -2,6 +2,9 @@ package com.example.projetoahoybackend.controller;
 
 import com.example.projetoahoybackend.service.SalvarDadosService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.Arrays;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class SalvarArquivoController {
 
     @Autowired
@@ -17,13 +21,15 @@ public class SalvarArquivoController {
 
 
     @PostMapping(value = "/uploadarquivo")
-    public void salvarArquivo(@RequestParam("file") MultipartFile file){
+    public ResponseEntity salvarArquivo(@RequestParam("file") MultipartFile file){
         salvarDadosService.processarArquivo(file);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("/uploadmultiplosarquivos")
-    public void uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
-         salvarDadosService.processarListaArquivos(files);
+    public ResponseEntity uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        salvarDadosService.processarListaArquivos(files);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
